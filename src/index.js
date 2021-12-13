81faaad7b85179e6e74456c1739711be71ab5c15
Jrjesus18 <juniorlopez3@live.com>
2021-12-13 00:12:25 -0500

    update

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Appcaja from './components/Appcaja'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+   <Appcaja />  
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
